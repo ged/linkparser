09ae54424d668b18b3fb325bad31239e53d29936
Michael Granger <ged@FaerieMUD.org>
2010-10-15 15:46:11 -0700

    Make the extconf use pkgconfig, if available, and add support for fat binary gems.

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -2,34 +2,89 @@
 
 require 'mkmf'
 
-ADDITIONAL_LIBRARY_DIRS = %w[
-	/usr/local/lib
-	/opt/lib
-	/opt/local/lib
-]
-ADDITIONAL_INCLUDE_DIRS = %w[
-	/usr/local/include
-	/opt/include
-	/opt/local/include
-]
-
-if ENV['DEBUG']
-	$CFLAGS << ' -Wall' << ' -DDEBUG'
+if lgdir = with_config( 'link-grammar' )
+	ENV['PATH'] = "#{lgdir}/bin" + File::PATH_SEPARATOR + ENV['PATH']
 end
 
-def fail( *messages )
-	$stderr.puts( *messages )
-	exit 1
+### Read the output of a command using the fork+pipe syntax so execution errors 
+### propagate to Ruby.
+def read_cmd_output( *cmd )
+	output = IO.read( '|-' ) or exec( *cmd )
+	return output.chomp
+end
+
+pkgconfig = with_config( 'pkg-config' ) || 'pkg-config'
+pkgconfig = find_executable( pkgconfig ) or
+	fail "Couldn't find your pkg-config binary"
+
+$LDFLAGS << read_cmd_output( pkgconfig, '--libs-only-L', 'link-grammar' )
+$CFLAGS << read_cmd_output( pkgconfig, '--cflags', 'link-grammar' )
+
+# Sort out the universal vs. single-archicture build problems on MacOS X
+if RUBY_PLATFORM.include?( 'darwin' )
+	puts "MacOS X build: fixing architecture flags:"
+
+	# Only keep the '-arch <a>' flags present in both the cflags reported
+	# by pkg-config and those that Ruby specifies.
+	commonflags = nil
+	if ENV['ARCHFLAGS']
+		puts "  using the value in ARCHFLAGS environment variable (%p)." % [ ENV['ARCHFLAGS'] ]
+		commonflags = ENV['ARCHFLAGS']
+	elsif pkgconfig
+		puts "  finding flags common to both Ruby and link-grammar..."
+		archflags = []
+		pkgcflags = read_cmd_output( pkgconfig, '--cflags', 'link-grammar' )
+		pkgcflags.scan( /-arch\s+(\S+)/ ).each do |arch|
+			puts "  testing for architecture: %p" % [ arch ]
+			archflags << "-arch #{arch}" if Config::CONFIG['CFLAGS'].index("-arch #{arch}")
+		end
+
+		commonflags = archflags.join(' ')
+		puts "  common arch flags: %s" % [ commonflags ]
+	else
+		$stderr.puts %{
+		===========   WARNING   ===========
+		
+		You are building this extension on OS X without setting the 
+		ARCHFLAGS environment variable, and pkg-config wasn't found in 
+		your PATH. If you are seeing this message, that means that the 
+		build will probably fail.
+
+		If it does, you can correct this by either including the path 
+		to 'pkg-config' in your PATH or setting the environment variable 
+		ARCHFLAGS to '-arch <arch>' before building.
+
+		For example:
+		(in bash) $ export PATH=/opt/local/bin:$PATH                  
+		          $ export ARCHFLAGS='-arch x86_64'
+		(in tcsh) % set path = ( /opt/local/bin $PATH )
+		          % setenv ARCHFLAGS '-arch x86_64'
+
+		Then try building again.
+
+		===================================
+		}.gsub( /^\t+/, '  ' )
+	end
+
+	if commonflags
+		$CFLAGS.gsub!( /-arch\s+\S+ /, '' )
+		$LDFLAGS.gsub!( /-arch\s+\S+ /, '' )
+		CONFIG['LDSHARED'].gsub!( /-arch\s+\S+ /, '' )
+
+		$CFLAGS << ' ' << commonflags
+		$LDFLAGS << ' ' << commonflags
+		CONFIG['LDSHARED'] << ' ' << commonflags
+	end
 end
 
 
 dir_config( 'link-grammar' )
 
 
-find_library( "link-grammar", "dictionary_create", *ADDITIONAL_LIBRARY_DIRS ) or
+find_library( "link-grammar", "dictionary_create" ) or
 	fail( "Could not find Link-Grammar library", 
 	      "(http://www.abisource.com/projects/link-grammar/#download)." )
-find_header( "link-grammar/link-includes.h", *ADDITIONAL_INCLUDE_DIRS ) or
+find_header( "link-grammar/link-includes.h" ) or
 	fail( "Could not find link-includes.h" )
 
 unless have_func( "linkage_free_diagram", 'link-grammar/link-includes.h' )
@@ -62,3 +117,4 @@ have_func( 'parse_options_get_spell_guess' )
 have_func( 'linkage_get_disjunct_str' )
 
 create_makefile( 'linkparser_ext' )
+
