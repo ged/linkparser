56b112770772874e83bbb8c1a4ec24ffe1c59aa5
Michael Granger <ged@FaerieMUD.org>
2011-01-11 10:18:12 -0800

    Avoid circular requires.
    --HG--
    extra : convert_revision : cdf1246eeab9efb842d3495d513854bf8df1db91

--- lib/linkparser.rb
+++ lib/linkparser.rb
@@ -14,7 +14,12 @@ module LinkParser
 	if RUBY_PLATFORM =~/(mswin|mingw)/i
 		major_minor = RUBY_VERSION[ /^(\d+\.\d+)/ ] or
 			raise "Oops, can't extract the major/minor version from #{RUBY_VERSION.dump}"
-		require "#{major_minor}/linkparser_ext"
+
+		begin
+			require "#{major_minor}/linkparser_ext"
+		rescue LoadError
+			require 'linkparser_ext'
+		end
 	else
 		require 'linkparser_ext'
 	end
